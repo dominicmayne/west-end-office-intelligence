842f12d2f53aaa249b2d8d36f106dccb88743511
Dom <dom@Doms-MBP.mynet>
2026-05-08 17:18:51 +0100

    prepare for Railway deployment

--- app.py
+++ app.py
@@ -68,3 +68,7 @@ def predictions():
     @app.get("/test")
 def test():
     return {"status": "working"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
